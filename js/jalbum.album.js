bd5a14172edbbf5a2a3cc3d8a53a64b24cfd1337
Omonar <adrian.kotelba@gmail.com>
2021-09-06 14:53:53 +0300

    Do not change file extenstion, yet.

--- js/jalbum.album.js
+++ js/jalbum.album.js
@@ -929,7 +929,7 @@ var Album = function($, options) {
 						makePath(getPath(o), o[J.ORIGINAL][J.PATH]) 
 						: 
 						((o.hasOwnProperty(J.CATEGORY) && o[J.CATEGORY] === 'image')? 
-							'/s3/photos/' + getFolderPath(o) + getName(o).replace('.webp','.jpg') 
+							'/s3/photos/' + getFolderPath(o) + getName(o) 
 							: 
 							null 
 						) 
